199fa4aa1c07318d202d8153daeb2516dd274845
Лаврентьев Даниил Александрович <d.lavrentev@rt-solar.ru>
2024-09-11 23:44:48 +0300

    add getSession itl

--- frontend/src/app/api/check/route.ts
+++ frontend/src/app/api/check/route.ts
@@ -1,15 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { getUserSession } from "@/shared/server";
 
 export async function GET(req: NextRequest) {
-  // If you don't have NEXTAUTH_SECRET set, you will have to pass your secret as `secret` to `getToken`
-  const token = await getToken({ req });
-  if (token) {
-    // Signed in
-    console.log("JSON Web Token", JSON.stringify(token, null, 2));
-    return NextResponse.json({ status: 200 });
-  } else {
-    // Not Signed in
-    return NextResponse.json({ status: 401 });
+  const userInfo = await getUserSession(req);
+
+  if (userInfo) {
+    return NextResponse.json({ status: 200, userInfo });
   }
+
+  return NextResponse.json({ status: 401 });
 }
---
+++ frontend/src/shared/server/helpers/getUserSession.ts
@@ -0,0 +1,23 @@
+import { getToken, JWT } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+
+type UserInfo = {
+  displayName: string;
+  picture: string | null;
+  name: JWT["name"];
+  email: JWT["email"];
+};
+
+export const getUserSession = async (
+  request: NextRequest
+): Promise<UserInfo | undefined> => {
+  const token = await getToken({ req: request });
+  if (token) {
+    return {
+      displayName: token.name || token.email || "Аноним",
+      name: token.name,
+      email: token.email,
+      picture: token?.picture ?? null,
+    };
+  }
+};

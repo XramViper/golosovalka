df84b058f487fd0eb103d7ed8fb37bd87c18cc70
Лаврентьев Даниил Александрович <d.lavrentev@rt-solar.ru>
2024-09-23 02:00:03 +0300

    Голосование за пост

--- frontend/src/entities/post/ui/upvote-button.tsx
+++ frontend/src/entities/post/ui/upvote-button.tsx
@@ -29,16 +29,19 @@ export const UpvoteButton = ({
     <button
       onClick={handleUpvote}
       disabled={isPending}
-      className={`px-4 py-2 rounded-box group text-center text-lg duration-150 border ${
-        isUpvoted
-          ? "border-transparent bg-primary text-primary-content"
-          : "border-base-content/10 bg-base-100 text-base-content"
-      }`}
+      className={`px-4 py-2 rounded-box group text-center text-lg duration-150 border
+        ${
+          isUpvoted
+            ? "border-transparent bg-primary text-primary-content"
+            : "border-base-content/10 bg-base-100 text-base-content"
+        }
+        ${isPending ? "opacity-50 cursor-not-allowed" : null}`}
       title="Проголосовать за пост"
     >
       <ChevronUpIcon
         strokeWidth="2.5"
-        className="w-5 h-5 ease-in-out duration-150 -translate-y-0.5 group-hover:translate-y-0"
+        className={`w-5 h-5 ease-in-out duration-150 -translate-y-0.5 
+          ${isPending ? "" : "group-hover:translate-y-0"}`}
       />
 
       {count}
